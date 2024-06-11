33249f42c2b79136b0f65d7df159bc428ccb02bb
Myles <mylesheller@icloud.com>
2024-06-11 16:59:26 -0400

    added dist

---
+++ dist/295.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkalignment_research_assistant=self.webpackChunkalignment_research_assistant||[]).push([[295],{295:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
